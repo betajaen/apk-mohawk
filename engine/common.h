e90ea0ce075e40cf81c9448fb227a5c2daae2a2c
Robin Southern <betajaen@users.noreply.github.com>
2024-01-28 06:56:34 +0000

    Initial placeholder files for Mohawk bitmap

---
+++ engine/common.h
@@ -0,0 +1,39 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef ENGINE_COMMON_H
+#define ENGINE_COMMON_H
+
+#include <apk/apk.h>
+
+using apk::uint;
+using apk::uint8;
+using apk::uint16;
+using apk::uint32;
+using apk::int8;
+using apk::int16;
+using apk::int32;
+
+typedef APK_SIZE_TYPE usize;
+typedef APK_SSIZE_TYPE isize;
+
+
+#endif
